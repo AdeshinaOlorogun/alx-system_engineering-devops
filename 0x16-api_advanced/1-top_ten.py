202cf3815e8e000cfd62057da6014e6ce0960d17
SparkZ <adeshinamolorogun@gmail.com>
2024-06-25 15:53:04 -0400

    Error Handling

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-    this module contains the function top_ten
+    This module contains the function top_ten
 '''
 import requests
 from sys import argv
@@ -8,17 +8,27 @@ from sys import argv
 
 def top_ten(subreddit):
     '''
-        returns the top ten posts for a given subreddit
+        Returns the top ten posts for a given subreddit
     '''
     user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+        response = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                                .format(subreddit), headers=user)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+        data = response.json()
+        
+        posts = data.get('data', {}).get('children', [])
+        if not posts:
+            print(None)
+        else:
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+    except requests.exceptions.RequestException:
+        print(None)
+    except ValueError:
         print(None)
 
 
 if __name__ == "__main__":
     top_ten(argv[1])
+
