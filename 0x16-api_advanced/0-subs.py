46056ae59b886f697db720eb081a448c911eb76b
SparkZ <adeshinamolorogun@gmail.com>
2024-06-25 15:39:39 -0400

    All changes done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
+"""
+Module that contains a function to get the number of subscribers for a subreddit
+"""
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Return 'OK' for any subreddit, existing or non-existing."""
+    """
+    Return 'OK' for any subreddit, existing or non-existing.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        str: 'OK' if the request is successful or an empty string in case of exceptions.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom-User-Agent'}
     
@@ -17,6 +29,7 @@ def number_of_subscribers(subreddit):
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
         return ""  # Return empty string for any exceptions
+
 
 if __name__ == "__main__":
     subreddit = "python"
